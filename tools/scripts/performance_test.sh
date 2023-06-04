27aa7505c9b45bccaa262a1d0db64a923a08e9bd
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2023-06-04 18:21:10 +0300

    Fix bugs

--- tools/scripts/performance_test.sh
+++ tools/scripts/performance_test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-./build_and_run_local_image.sh
+cd tests/performance || echo "Destination folder not found"
 
-
-
-docker run -v ${pwd}:/var/loadtest --rm --network host -it direvius/yandex-tank
+docker compose -d --file ./tests/performance/docker-compose.yml up
