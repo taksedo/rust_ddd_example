731c91f38cbd0eac8c7ba2710321c490f219e8bf
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2023-06-04 03:30:55 +0300

    Implement .env file

--- application/src/main/configuration/rest_configuration.rs
+++ application/src/main/configuration/rest_configuration.rs
@@ -2,22 +2,34 @@ use crate::main::configuration::use_case_configuration::{
     ADD_MEAL_TO_MEANU_USE_CASE, GET_MEAL_BY_ID_USE_CASE, GET_MENU_USE_CASE,
 };
 use actix_cors::Cors;
-use actix_web::http::header;
+use actix_web::http::{header, Uri};
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
 use rest::main::endpoint_url::{MENU_ADD_TO_MENU, MENU_GET_ALL, MENU_GET_BY_ID};
 use rest::main::menu::{
     add_meal_to_menu_endpoint, get_health_status, get_meal_by_id_endpoint, get_menu_endpoint,
 };
+use std::env;
+use std::net::IpAddr;
 use usecase::main::menu::scenario::add_meal_to_menu_use_case::AddMealToMenuUseCase;
 use usecase::main::menu::scenario::get_meal_by_id_use_case::GetMealByIdUseCase;
 use usecase::main::menu::scenario::get_menu_use_case::GetMenuUseCase;
 
 #[actix_web::main]
 pub async fn start_web_backend() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
+    dotenv().ok();
 
-    log::info!("starting HTTP server at http://localhost:8080");
+    env_logger::init_from_env(
+        env_logger::Env::new().default_filter_or(env::var("LOG_LEVEL").unwrap()),
+    );
+    log::info!("Log level is set to {:?}", env::var("LOG_LEVEL").unwrap());
+
+    let host_url = env::var("HOST_URL").unwrap().parse::<Uri>().unwrap();
+    let host_address = host_url.host().unwrap();
+    let host_port = host_url.port().unwrap();
+
+    log::info!("starting HTTP server at {}", env::var("HOST_URL").unwrap());
 
     HttpServer::new(move || {
         App::new()
@@ -26,7 +38,7 @@ pub async fn start_web_backend() -> std::io::Result<()> {
             .app_data(GET_MENU_USE_CASE.clone())
             .wrap(
                 Cors::default()
-                    .allowed_origin("http://localhost:8080")
+                    .allowed_origin(&env::var("HOST_URL").unwrap())
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
@@ -48,7 +60,15 @@ pub async fn start_web_backend() -> std::io::Result<()> {
             )
             .route("/health", web::get().to(get_health_status::execute))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((
+        host_address
+            .parse::<IpAddr>()
+            .expect("Wrong IP address configured"),
+        host_port
+            .to_string()
+            .parse::<u16>()
+            .expect("Wrong port configured"),
+    ))?
     .run()
     .await
 }
