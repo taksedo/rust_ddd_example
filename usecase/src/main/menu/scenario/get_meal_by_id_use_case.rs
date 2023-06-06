99e182dd01cded556c44c4c491bf8aa0a9a37f24
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2023-06-06 20:04:50 +0300

    Cosmetics

--- usecase/src/main/menu/scenario/get_meal_by_id_use_case.rs
+++ usecase/src/main/menu/scenario/get_meal_by_id_use_case.rs
@@ -13,7 +13,6 @@ pub struct GetMealByIdUseCase {
 impl GetMealById for GetMealByIdUseCase {
     fn execute(&mut self, id: MealId) -> Result<MealInfo, GetMealByIdUseCaseError> {
         self.meal_extractor.lock().unwrap().get_by_id(id);
-        // dbg!(&self.meal_extractor);
         match self.meal_extractor.lock().unwrap().get_by_id(id) {
             res if res.is_some() && res.clone().unwrap().visible() => {
                 let res = res.unwrap();
