e9fa07254a46a20df3d790fe501fc5092c1395a5
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2023-10-24 22:20:11 +0300

    fix

--- rest/src/main/endpoint_url.rs
+++ rest/src/main/endpoint_url.rs
@@ -5,7 +5,7 @@ pub const API_V1: &str = "/rest/shop/v1";
 pub const MENU: &str = formatcp!("{API_V1}/menu");
 pub const MENU_ADD_TO_MENU: &str = formatcp!("{MENU}/add");
 pub const MENU_GET_BY_ID: &str = formatcp!("{MENU}/{{id}}");
-pub const MENU_GET_ALL: &str = formatcp!("{MENU}/");
+pub const MENU_GET_ALL: &str = formatcp!("{MENU}");
 pub const MENU_DELETE_BY_ID: &str = formatcp!("{MENU}/{{id}}");
 
 pub const ORDER: &str = formatcp!("{API_V1}/orders");
