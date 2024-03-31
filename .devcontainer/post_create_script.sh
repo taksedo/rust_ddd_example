5972e927ea94f49fa0c40cc3daecbae721082b48
taksedo <48514012+taksedo@users.noreply.github.com>
2024-03-31 16:26:22 +0300

    Feature/fix devcontainer (#88)
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    * Minor fixes
    
    ---------
    
    Co-authored-by: Ruslan Valiev <ruslan_valiev@kamaz.ru>

---
+++ .devcontainer/post_create_script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+rustup default stable && \
+rustup component add rustfmt && \
+rustup component add clippy && \
+cargo install diesel_cli --no-default-features --features postgres && \
+cd shop && \
+diesel migration run && \
+cargo build
