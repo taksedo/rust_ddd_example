d7db10496f2296c31955addb6c5e4d2bcebc62e8
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2024-10-21 23:31:37 +0300

    Fix kafka

--- shop/application/src/event/mod.rs
+++ shop/application/src/event/mod.rs
@@ -1,11 +1,6 @@
 pub(super) mod event_publisher_impl;
 pub(super) mod integration_message_publisher;
-#[cfg(target_os = "linux")]
-#[path = "kafka_event_publisher_impl_linux.rs"]
-pub(super) mod kafka_event_publisher_impl_linux;
 
-#[cfg(target_os = "windows")]
-#[path = "kafka_event_publisher_impl_win.rs"]
 pub(super) mod kafka_event_publisher_impl;
 
 pub(super) mod rabbit_message_publisher;
