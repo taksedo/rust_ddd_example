26a5d3cc98e817e5c79de1c794c3bd116401319d
Ruslan Valiev <ruslan_valiev@kamaz.ru>
2024-12-04 08:33:43 +0300

    Refactor test_fixtures

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,6 +2,6 @@ pub use common_rest;
 pub use common_telnet;
 pub use events;
 
-#[cfg(feature = "testing")]
+#[cfg(all(test, feature = "testing"))]
 pub mod test_fixtures;
 pub use types;
--- shop/usecase/src/lib.rs
+++ shop/usecase/src/lib.rs
@@ -4,5 +4,5 @@ pub mod cart;
 pub mod menu;
 pub mod order;
 
-#[cfg(feature = "testing")]
+#[cfg(all(test, feature = "testing"))]
 pub mod test_fixtures;
